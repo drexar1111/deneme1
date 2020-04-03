9c8082207ef4a749bdd859591b20b4723a4bcdcb
ayhanmetehanaksoy <ayhanmetehanaksoy@hotmail.com.tr>
2020-04-04 00:18:02 +0300

    project pertama

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo ps = new ProcessStartInfo();
+
+            ps.Arguments = "-p -100 192.168.1.39";//processler
+            ps.RedirectStandardOutput = true;
+
+            ps.FileName = "nmap";//programın ismi
+            Process p = new Process();
+            p.StartInfo = ps;
+            p.Start();
+
+            StreamReader ayhan = p.StandardOutput;
+            StreamWriter sw = new StreamWriter("ayhan.txt");
+            sw.WriteLine(ayhan.ReadToEnd());
+
+            p.WaitForExit();
+
+            Console.WriteLine("Program Bitmiştir.");
+
+            Console.ReadKey(true);
+        }
+    }
+}
